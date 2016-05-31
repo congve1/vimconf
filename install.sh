3d45ad322d98322e84698600bb21eefb04d2a0bb
root <root@localhost.localdomain>
2016-05-31 12:19:53 +0800

    install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+cd vimconf
+Path=`pwd` 
+cd ~
+rm -rf .vim
+rm -rf .vimrc
+rm -rf .vimrc.locl
+rm -rf .vimrc.bundles
+rm -rf .vimrc.bundles.local
+ln -s $Path/vim ~/.vim
+ln -s $Path/vimrc ~/.vimrc
+ln -s $Path/vimrc.local ~/.vimrc.local
+ln -s $Path/vimrc.bundles ~/.vimrc.bundles
+ln -s $Path/vimrc.bundles.local ~/.vimrc.bundles.local
